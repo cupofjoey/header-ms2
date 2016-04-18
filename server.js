b6263ad32a1a3f997be07ff9fd8a011ebf600824
Joe Hill <joe@jkh3.com>
2016-04-18 19:55:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,26 @@ var app = express();
 var useragent = require('express-useragent');
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
-  var source = req.headers["user-agent"]
-  var ua = useragent.parse(source);	
+  try{
+  	var source = req.headers["user-agent"]
+  	var ua = useragent.parse(source);	
 
 	var metaData = {
 		"ipaddress" : req.ip,
 		"language" : req.acceptsLanguages()[0],
 		"software" : ua.os
 	};
-
-	//req.ip
-	//req.acceptsLanguages()[0]
-	//req.headers["operating system"]
-  //res.send(req.headers["os"]);
-
-  //res.writeHead(200, {'Content-Type': 'application/json'});
-  res.send(metaData);
+  	res.send(metaData);
+	}catch(err){
+		res.status(400).send(err);
+	}
 });
 
 app.listen(process.env.PORT || 5000);
